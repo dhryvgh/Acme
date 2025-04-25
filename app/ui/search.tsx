3c9c66b33fe620cb882ce27ed4f379f21c78f634
dhruygh <udhruv444@gmail.com>
2025-04-25 20:05:06 +0530

    fix

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { useDebouncedCallback } from 'use-debounce';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search() {
+type SearchProps = {
+  placeholder?: string;
+};
+
+export default function Search({ placeholder = 'Search...' }: SearchProps) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -28,7 +31,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder="placeholder"
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
