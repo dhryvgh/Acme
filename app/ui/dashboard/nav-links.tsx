8950486561b34d6275aa0a99f34cfb1bd642d01e
dhruygh <udhruv444@gmail.com>
2025-04-26 17:52:13 +0530

    final commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -20,7 +20,7 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', href: '/customers/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
